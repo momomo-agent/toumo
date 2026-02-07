5f18d6582d53bba6a99f5df67e7611704d1dc9f8
Kenefe Li <kenefe.li@gmail.com>
2026-02-07 09:35:19 +0800

    feat: add tutorial page with illustrations

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TutorialPage } from './components/TutorialPage.tsx'
+
+function Root() {
+  const hash = window.location.hash
+  if (hash === '#tutorial' || hash.startsWith('#tutorial?')) {
+    return <TutorialPage />
+  }
+  return <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
